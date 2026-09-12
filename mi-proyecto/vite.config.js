9cf667c50f285f96801c1ccdbaeac28770e9a1ea
1Paul2 <pollv3803@gmail.com>
2026-09-11 20:44:58 -0600

    Cambio del nombre del repo en el vite

--- mi-proyecto/vite.config.js
+++ mi-proyecto/vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PY1---Comercio-Electr-nico/',
+  base: '/PY1---Comercio-Electronico/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
